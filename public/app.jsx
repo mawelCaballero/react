2996bfa91bda2fee3f0947793aa48fc458e9a5cb
root <manuelcaballero@gmail.com>
2017-01-14 16:43:04 +0100

    React components properties example

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,29 @@
 var Greeter = React.createClass({
+  getDefaultProps: function()
+  {
+    return {
+        name: 'React',
+        message : 'This is a form component'
+    };
+  },
   render: function(){
-    return React.createElement('h1',null, 'Hello React.createElement')
+    var name  = this.props.name;
+    var message = this.props.message;
+    return (
+
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+      </div>
+    );
 
   }
 }); // most common react method
 
+var name = 'Mawel';
+var message = 'This is a custom message defined as var ';
 
 ReactDOM.render( // --> most common react dom method
-  <Greeter></Greeter>,
+  <Greeter name={name} message={message}></Greeter>,
   document.getElementById('app')
 );
