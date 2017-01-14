557f2ffb6833356486f942f666579bf08be1da09
root <manuelcaballero@gmail.com>
2017-01-14 17:22:15 +0100

    An state component example

--- public/app.jsx
+++ public/app.jsx
@@ -6,15 +6,24 @@ var Greeter = React.createClass({
         message : 'This is a form component'
     };
   },
+  getInitialState: function (){
+    return {
+        name: this.props.name
+
+    };
+  },
   onButtonClick: function(element){
     element.preventDefault();
-
     var name = this.refs.name.value;
-    alert(name);
+    this.refs.name.value = '';
+    this.setState({
+      name: name
+    });
 
   },
   render: function(){
-    var name  = this.props.name;
+    var name  =  this.state.name;
+
     var message = this.props.message;
     return (
 
